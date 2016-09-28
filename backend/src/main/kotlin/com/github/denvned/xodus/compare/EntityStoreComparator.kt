a6749e22d1aa3beec3f3205948857d8619039d85
Denis Nedelyaev <denvned@gmail.com>
2016-09-28 15:39:44 +0300

    Perform comparison in a separate thread

--- backend/src/main/kotlin/com/github/denvned/xodus/compare/EntityStoreComparator.kt
+++ backend/src/main/kotlin/com/github/denvned/xodus/compare/EntityStoreComparator.kt
@@ -4,7 +4,9 @@ import jetbrains.exodus.entitystore.*
 import jetbrains.exodus.env.EnvironmentConfig
 import jetbrains.exodus.env.Environments
 import java.util.*
+import java.util.concurrent.CompletableFuture
 import kotlin.comparisons.compareBy
+import kotlin.concurrent.thread
 
 object EntityStoreComparator {
     const val UPDATE_PROCESSED_BATCH_SIZE = 256
@@ -14,119 +16,108 @@ object EntityStoreComparator {
         newStoreLocation: EntityStoreLocation,
         store: PersistentEntityStore
     ): Entity {
-        fun getStore(storeLocation: EntityStoreLocation) = PersistentEntityStores.newInstance(
-            PersistentEntityStoreConfig().setRefactoringSkipAll(true),
-            Environments.newInstance(storeLocation.dir, EnvironmentConfig().setEnvIsReadonly(true).setGcEnabled(false)),
-            storeLocation.storeName
-        )
+        val futureComparison = CompletableFuture<Entity>()
 
-        val oldStore = getStore(oldStoreLocation)
-        val newStore = getStore(newStoreLocation)
-
-        try {
-            return compareStores(
-                oldStore = oldStore,
-                newStore = newStore,
-                store = store
+        thread {
+            fun getStore(storeLocation: EntityStoreLocation) = PersistentEntityStores.newInstance(
+                PersistentEntityStoreConfig().setRefactoringSkipAll(true),
+                Environments.newInstance(storeLocation.dir, EnvironmentConfig().setEnvIsReadonly(true).setGcEnabled(false)),
+                storeLocation.storeName
             )
-        } finally {
-            oldStore.close()
-            newStore.close()
-        }
-    }
 
-    fun compareStores(
-        oldStore: PersistentEntityStore,
-        newStore: PersistentEntityStore,
-        store: PersistentEntityStore
-    ): Entity {
-        val date = Date()
-        val oldTxn = oldStore.beginReadonlyTransaction()
-        val newTxn = newStore.beginReadonlyTransaction()
+            val oldStore = getStore(oldStoreLocation)
+            val newStore = getStore(newStoreLocation)
+            val oldTxn = oldStore.beginReadonlyTransaction()
+            val newTxn = newStore.beginReadonlyTransaction()
+            val date = Date()
 
-        try {
-            fun countEntities(txn: StoreTransaction): Long {
-                var count = 0L
-                txn.entityTypes.forEach { count += txn.getAll(it).size() }
-                return count
-            }
-
-            val comparison = store.computeInTransaction {
-                ComparisonEntityCreators.newComparison(
-                    txn = it,
-                    oldStoreDir = oldStore.location,
-                    oldStoreName = oldStore.name,
-                    newStoreDir = newStore.location,
-                    newStoreName = newStore.name,
-                    date = date,
-                    oldEntityCount = countEntities(oldTxn),
-                    newEntityCount = countEntities(newTxn)
-                )
-            }
-
-            var oldEntitiesProcessed = 0L
-            var newEntitiesProcessed = 0L
-
-            fun updateProcessed() {
-                store.executeInTransaction {
-                    ComparisonEntityCreators.updateComparisonProcessedCounts(
-                        comparison = comparison,
-                        oldEntitiesProcessed = oldEntitiesProcessed,
-                        newEntitiesProcessed = newEntitiesProcessed
-                    )
+            try {
+                fun countEntities(txn: StoreTransaction): Long {
+                    var count = 0L
+                    txn.entityTypes.forEach { count += txn.getAll(it).size() }
+                    return count
                 }
-            }
 
-            val oldTypeNames = oldTxn.entityTypes.associate { oldStore.getEntityTypeId(it) to it }
-            val newTypeNames = newTxn.entityTypes.associate { newStore.getEntityTypeId(it) to it }
-
-            val entityTypes = store.computeInTransaction {
-                (oldTypeNames.keys + newTypeNames.keys).associate { typeId ->
-                    val oldName = oldTypeNames[typeId]
-                    val newName = newTypeNames[typeId]
-
-                    typeId to ComparisonEntityCreators.newEntityType(
+                val comparison = store.computeInTransaction {
+                    ComparisonEntityCreators.newComparison(
                         txn = it,
-                        comparison = comparison,
-                        id = typeId,
-                        oldName = oldName,
-                        newName = newName
+                        oldStoreDir = oldStore.location,
+                        oldStoreName = oldStore.name,
+                        newStoreDir = newStore.location,
+                        newStoreName = newStore.name,
+                        date = date,
+                        oldEntityCount = countEntities(oldTxn),
+                        newEntityCount = countEntities(newTxn)
                     )
                 }
-            }
+                futureComparison.complete(comparison)
 
-            var i = 0L
-            for (typeId in entityTypes.keys) {
-                val oldEntities = oldTypeNames[typeId]?.let { oldTxn.getAll(it) }
-                val newEntities = newTypeNames[typeId]?.let { newTxn.getAll(it) }
+                var oldEntitiesProcessed = 0L
+                var newEntitiesProcessed = 0L
 
-                for ((oldEntity, newEntity) in MatchingIterator(oldEntities, newEntities, compareBy { it.id })) {
-                    EntityComparator(
-                        oldEntity = oldEntity,
-                        newEntity = newEntity,
-                        store = store,
-                        entityTypes = entityTypes
-                    ).compareEntities()
-
-                    if (oldEntity != null) {
-                        oldEntitiesProcessed++
-                    }
-                    if (newEntity != null) {
-                        newEntitiesProcessed++
-                    }
-
-                    if (++i % UPDATE_PROCESSED_BATCH_SIZE == 0L) {
-                        updateProcessed()
+                fun updateProcessed() {
+                    store.executeInTransaction {
+                        ComparisonEntityCreators.updateComparisonProcessedCounts(
+                            comparison = comparison,
+                            oldEntitiesProcessed = oldEntitiesProcessed,
+                            newEntitiesProcessed = newEntitiesProcessed
+                        )
                     }
                 }
+
+                val oldTypeNames = oldTxn.entityTypes.associate { oldStore.getEntityTypeId(it) to it }
+                val newTypeNames = newTxn.entityTypes.associate { newStore.getEntityTypeId(it) to it }
+
+                val entityTypes = store.computeInTransaction {
+                    (oldTypeNames.keys + newTypeNames.keys).associate { typeId ->
+                        val oldName = oldTypeNames[typeId]
+                        val newName = newTypeNames[typeId]
+
+                        typeId to ComparisonEntityCreators.newEntityType(
+                            txn = it,
+                            comparison = comparison,
+                            id = typeId,
+                            oldName = oldName,
+                            newName = newName
+                        )
+                    }
+                }
+
+                var i = 0L
+                for (typeId in entityTypes.keys) {
+                    val oldEntities = oldTypeNames[typeId]?.let { oldTxn.getAll(it) }
+                    val newEntities = newTypeNames[typeId]?.let { newTxn.getAll(it) }
+
+                    for ((oldEntity, newEntity) in MatchingIterator(oldEntities, newEntities, compareBy { it.id })) {
+                        EntityComparator(
+                            oldEntity = oldEntity,
+                            newEntity = newEntity,
+                            store = store,
+                            entityTypes = entityTypes
+                        ).compareEntities()
+
+                        if (oldEntity != null) {
+                            oldEntitiesProcessed++
+                        }
+                        if (newEntity != null) {
+                            newEntitiesProcessed++
+                        }
+
+                        if (++i % UPDATE_PROCESSED_BATCH_SIZE == 0L) {
+                            updateProcessed()
+                        }
+                    }
+                }
+
+                updateProcessed()
+            } finally {
+                oldTxn.abort()
+                newTxn.abort()
+                oldStore.close()
+                newStore.close()
             }
-
-            updateProcessed()
-
-            return comparison
-        } finally {
-            oldTxn.abort()
-            newTxn.abort()
         }
+
+        return futureComparison.get()
     }
 }
